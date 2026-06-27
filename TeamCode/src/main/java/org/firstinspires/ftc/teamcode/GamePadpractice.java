1a941d54590987ab6a667cd2d10142bf05fabfa9
BikiniBottomAdmin <lily.oliverluv@gmail.com>
2026-06-26 17:26:47 -0700

    Ifpractice.java (and another)
    this codes for is "if" practice. I added the "disabled" to the gamepad code because I forgot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadpractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadpractice.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@Disabled
+@Disabled //THIS CODE IS DISABLED!!
 @TeleOp
 public class GamePadpractice extends OpMode {
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfPractice.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled //THIS CODE IS DISABLED!!
+@TeleOp
+public class IfPractice extends OpMode {
+
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+       double motorSpeed = gamepad1.left_stick_y;
+
+      if (!gamepad1.a) {
+          motorSpeed *= 0.5;
+      }
+       telemetry.addData("Left Stick Value", motorSpeed);
+    }
+}
